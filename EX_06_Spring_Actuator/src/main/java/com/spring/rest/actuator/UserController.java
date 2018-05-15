029b1c611520d6bddfbfb393da81e19fb990a483
javasuresh91 <javasuresh91@gmail.com>
2018-05-15 17:29:22 +0530

    Added EX_06

---
+++ EX_06_Spring_Actuator/src/main/java/com/spring/rest/actuator/UserController.java
@@ -0,0 +1,66 @@
+package com.spring.rest.actuator;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.spring.rest.actuator.dto.User;
+import com.spring.rest.actuator.service.UserDaoService;
+
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	private UserDaoService userDao;
+	
+	//http://localhost:8080/users
+	@RequestMapping(method=RequestMethod.GET,path="/users")
+	public List<User> getAllUsers() {
+		return userDao.findAll();
+	}
+	
+	//http://localhost:8080/user/1
+	@GetMapping("/user/{id}")
+	public User getUserInfo(@PathVariable(name="id") int userId) {
+		return userDao.findUser(userId);
+	}
+	
+	//http://localhost:8080/addUser
+	@PostMapping("/addUser")
+	public void createUser(@RequestBody User user) {
+		userDao.add(user);
+	}
+	
+	//http://localhost:8080/addUserGetInfo
+	@PostMapping("/addUserGetInfo")
+	public ResponseEntity<String> createUserGetInfo(@RequestBody User user) {
+		User savedUser = userDao.add(user);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).body("User Created");
+		
+	}
+	
+	//http://localhost:8080/deleteUser/1
+	@DeleteMapping("/deleteUser/{userId}")
+	public void deleteUser(@PathVariable int userId) {
+		User user = userDao.deleteUser(userId);
+		
+	}
+	
+	
+
+}
---
+++ EX_06_Spring_Actuator/src/main/java/com/spring/rest/actuator/dto/User.java
@@ -0,0 +1,82 @@
+package com.spring.rest.actuator.dto;
+
+import java.util.Date;
+
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+	private Integer id;
+	@Size(min = 2, max = 10)
+	private String name;
+	@Past
+	private Date dob;
+
+	private User() {
+	}
+
+	public User(Integer id, String name, Date dob) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dob = dob;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the dob
+	 */
+	public Date getDob() {
+		return dob;
+	}
+
+	/**
+	 * @param dob
+	 *            the dob to set
+	 */
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", dob=" + dob + "]";
+	}
+}
