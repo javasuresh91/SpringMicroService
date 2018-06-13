d2b7922f835e947af3e61e163e6d77f821a0461d
Suresh R <Sravikumar@altimetrik.com>
2018-06-13 11:20:57 +0530

    updated EX_13

--- EX_13_MS_CurrenyConversionService/src/main/java/com/demo/microservice/EX_13_MS_CurrenyConversionService/Ex13MsCurrenyConversionServiceApplication.java
+++ EX_13_MS_CurrenyConversionService/src/main/java/com/demo/microservice/EX_13_MS_CurrenyConversionService/Ex13MsCurrenyConversionServiceApplication.java
@@ -2,17 +2,25 @@ package com.demo.microservice.EX_13_MS_CurrenyConversionService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
+@SpringBootApplication
 /**
  * 
  * Need to enable the feign Client to talk to external micro service and pass the
  * package name for the scanning
  *
  */
-@SpringBootApplication
 @EnableFeignClients("com.demo.microservice.EX_13_MS_CurrenyConversionService")
+
+/**
+ * 
+ * @author Sravikumar
+ * This Currency conversion application needs to register with the Naming Server
+ */
+@EnableDiscoveryClient
 public class Ex13MsCurrenyConversionServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Ex13MsCurrenyConversionServiceApplication.class, args);
